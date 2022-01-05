a7e77069156fe722a278da2b7eaee1b254db42ca
Mark <snapstil1eren@gmail.com>
2022-01-05 09:44:17 +0100

    Add project files.

---
+++ 1Semprojekt2022_Golf/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _1Semprojekt2022_Golf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
